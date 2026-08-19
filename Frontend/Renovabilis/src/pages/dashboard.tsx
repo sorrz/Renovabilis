b4f3969b1da83f89ad03e8c76d356a62a07cd4a3
linasidani <linaiskander@hotmail.com>
2026-08-19 18:54:56 +0200

    Återställ elkartan

--- Frontend/Renovabilis/src/pages/dashboard.tsx
+++ Frontend/Renovabilis/src/pages/dashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Container, Typography, Button, Stack } from '@mui/material';
 
 export default function Dashboard() {
